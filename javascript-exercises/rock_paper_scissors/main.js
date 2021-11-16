e3bc0f5e9989e71d13d099baebd44ce8195a89d4
David Wellsandt <david@wellsandt.net>
2021-11-16 00:39:10 -0600

    Rock Paper Scissors - 5 round play completed.

--- javascript-exercises/rock_paper_scissors/main.js
+++ javascript-exercises/rock_paper_scissors/main.js
@@ -4,23 +4,44 @@ console.log('Welcome to Rock-Paper-Scissors!')
 const OPTIONS = ['Paper', 'Rock', 'Scissors'];
 
 
-// Get user selection
-let playerSelection = toTitleCase(prompt("Please enter your choice."));
-console.log(`playerSelection: %s`, playerSelection);
-
-let playerSelectionID = OPTIONS.indexOf(playerSelection);
-console.log(`playerSelectionID: %s`, playerSelectionID);
+game();
 
 
-// Get computer selection
-let computerSelection = computerPlay();
-console.log(`computerSelection: %s`, computerSelection);
+// run the game for 5 rounds
+function game() {
+    let player_wins = 0;
+    let computer_wins = 0;
+    for (let ctr=0; ctr < 5; ctr++) {
 
-let computerSelectionID = OPTIONS.indexOf(computerSelection);
-console.log(`computerSelectionID: %s`, computerSelectionID);
+        // Get user selection
+        let playerSelection = toTitleCase(prompt("Please enter your choice."));
+        console.log(`playerSelection: %s`, playerSelection);
+
+        let playerSelectionID = OPTIONS.indexOf(playerSelection);
+        console.log(`playerSelectionID: %s`, playerSelectionID);
 
 
-console.log(playRound(playerSelectionID, computerSelectionID));
+        // Get computer selection
+        let computerSelection = computerPlay();
+        console.log(`computerSelection: %s`, computerSelection);
+
+        let computerSelectionID = OPTIONS.indexOf(computerSelection);
+        console.log(`computerSelectionID: %s`, computerSelectionID);
+
+
+        result = playRound(playerSelectionID, computerSelectionID);
+        if (result == 1) {
+            player_wins++;
+        }
+        else if (result == -1) {
+            computer_wins++;
+        }
+    }
+    console.log('GAME OVER! After 5 rounds...');
+    let ties = 5 - player_wins - computer_wins;
+    console.log(`Player won ${player_wins}, Computer won ${computer_wins}, and ${ties} tie rounds.`);
+}
+
 
 
 function toTitleCase(str) {
@@ -38,16 +59,20 @@ function computerPlay() {
 
 
 // Provide the OPTIONS id to the round, not the string value
+// For each round, return 1 for player win, -1 for computer, and 0 if a tie
 function playRound(playerSelectionID, computerSelectionID) {
     // If values match, it's a tie
     if (playerSelectionID == computerSelectionID) {
-        return `Both players choose ${OPTIONS[playerSelectionID]}; it's a tie.`;
+        console.log(`Both players choose ${OPTIONS[playerSelectionID]}; it's a tie.`);
+        return 0;
     }
     // Else If Player < Computer OR Player is Scissors and Computer is Paper, player wins
     else if ( ( playerSelectionID < computerSelectionID ) || ( (playerSelectionID == 2) && ( computerSelectionID == 0) ) ) {
-        return `Player wins! ${OPTIONS[playerSelectionID]} beats ${OPTIONS[computerSelectionID]}`;
+        console.log(`Player wins! ${OPTIONS[playerSelectionID]} beats ${OPTIONS[computerSelectionID]}`);
+        return 1;
     }
     else {
-        return `Computer wins! ${OPTIONS[computerSelectionID]} beats ${OPTIONS[playerSelectionID]}`;
+        console.log(`Computer wins! ${OPTIONS[computerSelectionID]} beats ${OPTIONS[playerSelectionID]}`);
+        return -1;
     }
 }
